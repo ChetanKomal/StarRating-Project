e8900c36824e0d477dfd25643272c173e8a825bf
Chetan Komal <76816799+ChetanKomal@users.noreply.github.com>
2021-10-20 10:50:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Starrating from './Starrating';
+
+function App() {
+  return (
+    <div className="App">
+    <Starrating/>
+    </div>
+  );
+}
+
+export default App;
